6adc5b80698c77e451ce6f7b73e6090bd8365020
Italo Nunes Leite <italoleite08@hotmail.com>
2019-07-23 00:14:55 -0300

    - Inserindo botão e função Sortear Imagens

--- game.js
+++ game.js
@@ -17,8 +17,8 @@ var _imagens = [
         url: "http://lorempixel.com/200/200/sports/3/"
     },
     {
-        id: 4,
-        titulo: 'imagem 44',
+        id: 1,
+        titulo: 'imagem 1',
         url: "http://lorempixel.com/200/200/sports/1/"
     },
 ]
@@ -26,12 +26,12 @@ new Vue({
     el: '#app',
     data: {
         imagens: _imagens,
-       
-        
+
+
     },
     computed: {
-     
-        
+
+
     },
     methods: {
         selecionarImg: function (imagem) {
@@ -39,18 +39,22 @@ new Vue({
             if (localStorage.getItem("IdImagem") === null) {
                 //armazenando id no localStorage
                 localStorage.setItem("IdImagem", imagem);
-           //comparando o id da imagem com localstorage
+                //comparando o id da imagem com localstorage
             } else if (localStorage.getItem("IdImagem") == imagem) {
                 alert('é igual');
                 localStorage.clear();
-            } else { 
+            } else {
                 alert('nao é igual')
                 localStorage.clear();
             }
-            
-           
-            
-            
+        },
+        SortearImagens: function () {
+            debugger
+            var div = $("#sortearimagens");
+            var lis = div.children();
+            var sorteada = Math.floor(Math.random() * lis.length);
+            for (var i = 0; i < sorteada; i++)
+                lis.eq(i).detach().appendTo(div);
         }
     }
 }); 
